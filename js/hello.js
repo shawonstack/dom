32984037a106294d56a8a9e1e4b25dcb57b814fe
shawonstack <shawon934764@gmail.com>
2026-05-03 16:22:36 +0600

    multiple tag change using class name

--- js/hello.js
+++ js/hello.js
@@ -6,12 +6,22 @@ const bio = {
     console.log('his hobby is playing ', play);
   },
 };
-const liItem = document.getElementsByTagName('li');
-for (const li of liItem) {
-  console.log(li.innerText);
-}
+// const liItem = document.getElementsByTagName('li');
+// for (const li of liItem) {
+//   console.log(li.innerText);
+// }
 
-const newList = document.getElementsByTagName('li');
-for (list of newList) {
-  console.log(list.innerText);
+// const newList = document.getElementsByTagName('li');
+// for (list of newList) {
+//   console.log(list.innerText);
+// }
+
+const day = document.getElementById('morning');
+console.log(
+  (day.innerHTML = 'Early wake up person has more powerfull than others'),
+);
+
+const multipleChange = document.getElementsByClassName('mgs');
+for (change of multipleChange) {
+  console.log((change.innerText = 'pragraph'));
 }
