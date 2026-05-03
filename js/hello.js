a8bba83622b4b5bb6d5fd8937bedcd61aac4ae7f
shawonstack <shawon934764@gmail.com>
2026-05-04 01:39:54 +0600

    creat element and share what is my dream

--- js/hello.js
+++ js/hello.js
@@ -1,36 +1,36 @@
-// const bio = {
-//   name: 'abul mia',
-//   id: 3344,
-//   study: 'cse',
-//   hobby: function (play) {
-//     console.log('his hobby is playing ', play);
-//   },
-// };
-// const liItem = document.getElementsByTagName('li');
-// for (const li of liItem) {
-//   console.log(li.innerText);
-// }
+const bio = {
+  name: 'abul mia',
+  id: 3344,
+  study: 'cse',
+  hobby: function (play) {
+    console.log('his hobby is playing ', play);
+  },
+};
+const liItem = document.getElementsByTagName('li');
+for (const li of liItem) {
+  console.log(li.innerText);
+}
 
-// const newList = document.getElementsByTagName('li');
-// for (list of newList) {
-//   console.log(list.innerText);
-// }
+const newList = document.getElementsByTagName('li');
+for (list of newList) {
+  console.log(list.innerText);
+}
 
-// const day = document.getElementById('morning');
-// console.log(
-//   (day.innerHTML = 'Early wake up person has more powerfull than others'),
-// );
+const day = document.getElementById('morning');
+console.log(
+  (day.innerHTML = 'Early wake up person has more powerfull than others'),
+);
 
-// const multipleChange = document.getElementsByClassName('mgs');
-// for (change of multipleChange) {
-//   console.log((change.innerText = 'pragraph'));
-// }
+const multipleChange = document.getElementsByClassName('mgs');
+for (change of multipleChange) {
+  console.log((change.innerText = 'pragraph'));
+}
 
-// const sectionAll = document.querySelector('.section-container p');
-// console.log(
-//   (sectionAll.innerText =
-//     'morning shows the days, because  if any person ge early in the morning, he will get too much time than othes, and he anage and utilize those time proprly'),
-// );
+const sectionAll = document.querySelector('.section-container p');
+console.log(
+  (sectionAll.innerText =
+    'morning shows the days, because  if any person ge early in the morning, he will get too much time than othes, and he anage and utilize those time proprly'),
+);
 const p = document.getElementById('text');
 console.log(p.parentNode);
 p.parentNode.style.background = 'blue';
@@ -39,3 +39,19 @@ const creatH1 = document.createElement('h1');
 creatH1.innerText = 'Creat h1 using javascript';
 const div = document.getElementById('shawon');
 div.appendChild(creatH1);
+
+const mainSection = document.getElementById('main-section');
+const visitPlaces = document.createElement('section');
+visitPlaces.innerHTML = `
+<h1>What is my actual dream? </h1>
+<p>My actual dram is, i will visit so many beautiful places, plces can be sea, the mountain view, the snow covered landscaps. I think they just have a different kind of magic. me my mom and dad an my wife that want to visit these places. i also see ashes in australia with my dad. my dad loves ashes so much. </p>
+<ul>
+<li>Mekka</li>
+<li>Madina</li>
+<li>Australia</li>
+<li>japan</li>
+<li>nepal</li>
+<li>maldip</li>
+</ul>
+`;
+mainSection.appendChild(visitPlaces);
