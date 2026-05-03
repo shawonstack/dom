9623b03d17649cdaafbc16bd559d117746ddef72
shawonstack <shawon934764@gmail.com>
2026-05-03 15:48:34 +0600

    get element by tag nam  cover

--- js/hello.js
+++ js/hello.js
@@ -1,2 +1,17 @@
-const item = document.getElementsByTagName('li');
-console.log(item);
+const bio = {
+  name: 'abul mia',
+  id: 3344,
+  study: 'cse',
+  hobby: function (play) {
+    console.log('his hobby is playing ', play);
+  },
+};
+const liItem = document.getElementsByTagName('li');
+for (const li of liItem) {
+  console.log(li.innerText);
+}
+
+const newList = document.getElementsByTagName('li');
+for (list of newList) {
+  console.log(list.innerText);
+}
