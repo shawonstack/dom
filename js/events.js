93ccdf23802f40f1286b7599484c29c3509589ab
shawonstack <shawon934764@gmail.com>
2026-05-04 12:22:58 +0600

    creat btn and coor change wth event handeler

--- js/events.js
+++ js/events.js
@@ -3,3 +3,18 @@ const btnRed = document.getElementById('btn-red');
 btnRed.onclick = function makeRed() {
   document.body.style.backgroundColor = 'red';
 };
+
+const btnGreen = document.getElementById('btn-green');
+btnGreen.onclick = function green() {
+  document.body.style.backgroundColor = '#2B8C2B';
+};
+
+const btnYellow = document.getElementById('btn-yellow');
+btnYellow.onclick = function yellow() {
+  document.body.style.backgroundColor = '#C4BB2D';
+};
+
+const btnSubscribe = document.getElementById('subscribe');
+btnSubscribe.onclick = function subs() {
+  document.body.style.backgroundColor = '#168277';
+};
