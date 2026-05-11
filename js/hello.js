183a8aced1bdb86457c2e145752fcfbcec262797
shawonstack <shawon934764@gmail.com>
2026-05-11 15:43:41 +0600

    task 2 is done

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,15 @@
-const items = document.getElementsByClassName('menu-item');
-for (const item of items) {
-  const newItem = item;
-  console.log(newItem);
-  newItem.style.color = 'red';
-}
+// const items = document.getElementsByClassName('menu-item');
+// for (const item of items) {
+//   const newItem = item;
+//   console.log(newItem);
+//   newItem.style.color = 'red';
+// }
+
+let buttonName = document
+  .getElementById('btn-container')
+  .addEventListener('click', function () {
+    let heading = document.getElementById('h1-container');
+    document.body.style.backgroundColor = 'red';
+    heading.style.color = 'white';
+    heading.innerText = 'heading color goes to white';
+  });
